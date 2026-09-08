8465c0090b674d21f2aeae95afa5033597e93b00
Ankitame1 <ankitakumari8069@gmail.com>
2026-09-08 07:37:42 +0530

    Palindrome

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+print("Enter a number: ")
+number = int(input())
+new_number = number
+result = 0
+sum = 0
+
+while(number > 0):
+    result = number%10
+    print("Result: ",result)
+    sum = sum * 10 + result
+    print("sum: ",sum)
+    number = number//10
+    print("number: ",number)
+
+if (new_number == sum):
+    print("IT is palindrome.")
+else :
+    print("Not a palindrome")
