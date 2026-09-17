57f0e43e04d18652f39d0f32f4e40af8d6ee6956
Ankitame1 <ankitakumari8069@gmail.com>
2026-09-17 10:52:42 +0530

    Add Bubble Sort.

---
+++ bubblesort.py
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {5, 1, 4, 2, 8};
+
+    int n = 5;
+
+    for(int i=0;i < n-1; i++){
+        for (int j = 0; j < n-1-i; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for( int k = 0; k < n; k++){
+        cout << arr[k];
+    }
+
+    return 0;
+    
+
+}
